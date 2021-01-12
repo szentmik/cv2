bb4e562185a68029670170b58c1a3b8a1681d24f
adam <szentmik@gmail.com>
2021-01-12 17:04:36 +0100

    menu update

---
+++ index.js
@@ -0,0 +1,7 @@
+function showModal() {
+  document.querySelector("#menu-modal").style.display=("block");
+}
+
+function hideModal() {
+  document.querySelector("#menu-modal").style.display=("none");
+}
