df736d8809fce39a996827eed530848e44f03668
adam <szentmik@gmail.com>
2021-01-14 13:50:22 +0100

    wrote java script, to rotate and transform in mobile view the menu bar.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+var icon = document.querySelector("#menu-icon");
+var menuModal = document.querySelector("#menu-modal");
+
+// Show the menu
+
 function showModal() {
-  document.querySelector("#menu-modal").style.display=("block");
+    menuModal.classList.toggle("showMeMenu");
+
+    icon.classList.add("rotate-item");
+    setTimeout(function() {
+      icon.classList.remove("rotate-item");}, 310);
+
 }
 
-function hideModal() {
-  document.querySelector("#menu-modal").style.display=("none");
-}
+// Icon Change
+
+icon.addEventListener("click" , function(event){
+  var class2 = event.target.classList[1];
+  var iconChange = icon.classList;
+
+  if (class2 === "fa-bars") {
+    iconChange.replace("fa-bars", "fa-times");
+  }
+  else {
+    iconChange.replace("fa-times", "fa-bars");
+  }
+  console.log(class2);
+});
